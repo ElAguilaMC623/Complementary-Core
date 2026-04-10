e8198717ff78e4c3f3523492bd33b6c5a569a825
elaguilamc623@gmail.com <elaguilamc623@gmail.com>
2026-04-10 23:20:04 +0200

    Removed the unused config of the mod

--- src/main/java/net/elaguilamc623/complementary_core/ComplementaryCoreClient.java
+++ src/main/java/net/elaguilamc623/complementary_core/ComplementaryCoreClient.java
@@ -24,8 +24,5 @@ public class ComplementaryCoreClient {
 
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
-        // Some client setup code
-        ComplementaryCore.LOGGER.info("HELLO FROM CLIENT SETUP");
-        ComplementaryCore.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
